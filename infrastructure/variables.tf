09f5db0b11116f00d0c24c8b30b030fdc054a577
Lukas Bernhard <strange85@web.de>
2025-01-14 18:21:35 +0100

    fix zone

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,6 +1,6 @@
 variable "route53_zone_id" {
   description = "Route53 Zone ID for lukas-bernhard.de"
-  default = "lukas.bernhard.de"
+  default = "lukas-bernhard.de"
 }
 
 variable "key_pair_name" {
